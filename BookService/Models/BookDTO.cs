506e450e02ee13282a443702f8d8466155a2b9e7
Jens Christian Rasch <jcr@wodecraft.dk>
2016-11-23 10:29:01 +0100

    Exercise 6 - Added DTO objects for returning data. Modified the BooksController to use the DTO objects.

---
+++ BookService/Models/BookDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace BookService.Models
+{
+    public class BookDTO
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+
+    }
+}
